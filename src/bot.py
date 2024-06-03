4911c8c3ad5d145f5dc9f4d874c87e4e4456058d
Alvis <37454764+avibn@users.noreply.github.com>
2024-06-03 12:22:05 +0100

    add download confirmation

--- src/bot.py
+++ src/bot.py
@@ -4,9 +4,10 @@ import shutil
 import time
 
 from dotenv import load_dotenv
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Application,
+    CallbackQueryHandler,
     CommandHandler,
     ContextTypes,
     MessageHandler,
@@ -34,16 +35,39 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
+# List of available commands
+commands = {
+    "/start": "Start the bot",
+    "/help": "Get help",
+    "/info": "Get user and chat info",
+}
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    commands_list = "The following commands are available:\n" + "\n".join(
+        [f"{key} - {value}" for key, value in commands.items()]
+    )
+    await update.message.reply_text(
+        f"{commands_list}\n\nSend me a file and I'll download it to `{DOWNLOAD_TO_DIR}`.",
+        parse_mode="markdown",
+    )
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+        f"Hi {user.mention_html()}! I'm a bot that can download files for you. "
+        "Send me a file and I'll download it for you.\n\n"
+        "Use /help to see available commands."
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help!")
+async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    await update.message.reply_text(
+        f"*User ID*: {user.id}\n*Chat ID*: {update.effective_chat.id}",
+        parse_mode="markdown",
+    )
 
 
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -59,42 +83,79 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     file_name = update.message.document.file_name
     file_size = update.message.document.file_size / 1024 / 1024  # in MB
 
-    # Send start message
-    await update.message.reply_text(
-        f"Downloading file...\nFile name: {file_name}\nFile size: {file_size} MB"
+    # Confirmation message
+    ok = await update.message.reply_text(
+        f"Download file?\nFile name: {file_name}\nFile size: {file_size:.2f} MB",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Yes", callback_data="yes"),
+                    InlineKeyboardButton("No", callback_data="no"),
+                ]
+            ]
+        ),
     )
-    start_time = time.time()
 
-    try:
-        newFile = await context.bot.get_file(file_id, read_timeout=1000)
-    except Exception as e:
-        await update.message.reply_text(f"File size: {file_size}\nError: {e}")
-        return
 
-    # Work out time taken to download file
-    end_time = time.time()
-    download_time = end_time - start_time
-    file_path = newFile.file_path.split("/")[
-        -1
-    ]  # "./documents/file_1.mkv" to "file_1.mkv"
-    downloaded_file_size = newFile.file_size / 1024 / 1024  # in MB
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info("Button command received")
+    query = update.callback_query
 
-    # Rename the file to the original file name
-    current_file_path = f"{BOT_API_DIR}{TOKEN_SUB_DIR}/documents/{file_path}"
-    move_to_path = f"{DOWNLOAD_TO_DIR}{file_name}"
+    await query.answer()
 
-    # Make DOWNLOAD_TO_DIR if it doesn't exist
-    os.makedirs(DOWNLOAD_TO_DIR, exist_ok=True)
-    shutil.move(current_file_path, move_to_path)
+    # Replied to message
+    message = update.effective_message.reply_to_message
+    file_name = message.document.file_name
+    file_size = message.document.file_size / 1024 / 1024  # in MB
 
-    await update.message.reply_text(
-        f"""
-File downloaded successfully.\n
-Time taken:    {download_time:.5f} seconds\n
-File path:     {file_path}\n
-File name:     {file_name}\n
-File size:     {downloaded_file_size:.2f} MB"""
-    )
+    if query.data == "yes":
+        logger.info("Downloading file...")
+
+        # Check if file exists in DOWNLOAD_TO_DIR already
+        if os.path.exists(DOWNLOAD_TO_DIR + file_name):
+            await message.reply_text("File already exists in downloads folder.")
+            return
+
+        await message.reply_text(
+            f"Downloading file...\nFile name: {file_name}\nFile size: {file_size:.2f} MB"
+        )
+        start_time = time.time()
+
+        try:
+            newFile = await context.bot.get_file(
+                message.document.file_id, read_timeout=1000
+            )
+        except Exception as e:
+            await message.reply_text(f"File size: {file_size}\nError: {e}")
+            return
+
+        # Work out time taken to download file
+        end_time = time.time()
+        download_time = end_time - start_time
+        download_time_mins = download_time / 60
+        file_path = newFile.file_path.split("/")[-1]
+        downloaded_file_size = newFile.file_size / 1024 / 1024
+
+        # Rename the file to the original file name
+        current_file_path = f"{BOT_API_DIR}{TOKEN_SUB_DIR}/documents/{file_path}"
+        move_to_path = f"{DOWNLOAD_TO_DIR}{file_name}"
+
+        # Make DOWNLOAD_TO_DIR if it doesn't exist
+        os.makedirs(DOWNLOAD_TO_DIR, exist_ok=True)
+        shutil.move(current_file_path, move_to_path)
+
+        response_message = (
+            f"File downloaded successfully.\n"
+            f"Time taken:    {download_time:.5f} secs ({download_time_mins:.2f} mins)\n"
+            f"File path:     {file_path}\n"
+            f"File name:     {file_name}\n"
+            f"File size:     {downloaded_file_size:.2f} MB"
+        )
+
+        await message.reply_text(response_message)
+    else:
+        logger.info("Download cancelled")
+        await message.reply_text("Download cancelled.")
 
 
 def main() -> None:
@@ -111,9 +172,11 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("info", info))
 
     # on file upload - download the file
     application.add_handler(MessageHandler(filters.Document.VIDEO, download))
+    application.add_handler(CallbackQueryHandler(button))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
